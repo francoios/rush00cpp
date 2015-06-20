7c71dba59f08356c9e3805c9c214104929b921d6
Quentin <quentin.muntadas@orange.fr>
2015-06-20 16:10:02 +0200

    update

---
+++ includes/GameCore.Class.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   GameCore.Class.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qmuntada <qmuntada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/06/20 15:56:43 by qmuntada          #+#    #+#             */
+/*   Updated: 2015/06/20 16:07:39 by qmuntada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAMECORE_CLASS_HPP
+# define GAMECORE_CLASS_HPP
+# include <iostream>
+class GameCore	{
+public:
+	GameCore();
+	GameCore(GameCore const &rhs);
+	~GameCore();
+
+private:
+
+protected:
+
+};
+#endif
