59081b212a0e411408fc14905b86577c0dbdddde
Quentin <quentin.muntadas@orange.fr>
2015-06-20 16:27:15 +0200

    maj

--- includes/GameCore.Class.hpp
+++ includes/GameCore.Class.hpp
@@ -6,22 +6,37 @@
 /*   By: qmuntada <qmuntada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/20 15:56:43 by qmuntada          #+#    #+#             */
-/*   Updated: 2015/06/20 16:07:39 by qmuntada         ###   ########.fr       */
+/*   Updated: 2015/06/20 16:24:13 by qmuntada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAMECORE_CLASS_HPP
 # define GAMECORE_CLASS_HPP
+
 # include <iostream>
-class GameCore	{
+
+# include "Ft_Time.Class.hpp"
+# include "Screen.Class.hpp"
+
+class GameCore
+{
 public:
 	GameCore();
 	GameCore(GameCore const &rhs);
 	~GameCore();
+
+	GameCore const &	operator=(GameCore const & rhs);
+	void				Update(void);
+
+	//Screen		screen; // Window handling
+	//Ft_Time		ftime; // Time/frame handling
+	// ICI TOUTES LES CLASSE PRATIQUE COMME SCREEN, TIME, QUI
+	// SERONT AINSI ACCESSIBLE GRACE A L'INSTANCE DE GAMECORE
 
 private:
 
 protected:
 
 };
+
 #endif
